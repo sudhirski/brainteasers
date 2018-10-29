25df9eefb3eae731febb66af3460000e16518473
sudhirski <crv713@gmail.com>
2018-10-28 17:27:56 -0700

    Add files via upload
    Recursively reverse a string

---
+++ RecursiveReverse.java
@@ -0,0 +1,31 @@
+package oracle.idaas.common.util;
+
+import com.google.common.base.Strings;
+
+/**
+ * @authod Sudhir Kumar.
+ *
+ * Class to recursively reverse a string.
+ */
+public class RecursiveReverse {
+
+    private final static String toReverse = "ABCDE";
+
+    public static void main(String[] args) {
+
+        System.out.println("Reversed Form of " + toReverse + " is : " + recursiveReverse(toReverse));
+
+    }
+
+    static String recursiveReverse(String str) {
+
+        if (Strings.isNullOrEmpty(str)) {
+            return "";
+        }
+
+        char thisChar = str.charAt(0); // Isolate the first character
+        String remaining = str.substring(1); // The remaining portion of the string except the first character
+        
+        return recursiveReverse(remaining)  + thisChar;
+    }
+}
